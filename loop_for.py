ec59c923e9e5c40ad9b53d34fa0ac9923a66e00b
Auguste <longwonderstaff@gmail.com>
2026-04-15 00:06:14 +0300

    *loop for* task #13

--- loop_for.py
+++ loop_for.py
@@ -85,6 +85,20 @@ def Loops_Task6():
     for i in range(m, n - 1, -2):
         print(i)
 
+def Loops_Task7():
+# Даны два целых числа m и n. 
+# Напишите программу, которая выводит все целые числа от m до n включительно в порядке возрастания, 
+# если m < n, или в порядке убывания в противном случае.
+
+    m, n = int(input()), int(input())
+
+    if m < n:
+        for i in range(m, n, 1):
+            print(i)
+    elif m > n:
+        for i in range(m, n, -1):
+            print(i)
+
 
 
 

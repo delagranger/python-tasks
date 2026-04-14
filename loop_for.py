b97cf7f53922c713f7564a8d829bacd0af6f3bf0
Auguste <longwonderstaff@gmail.com>
2026-04-15 00:52:18 +0300

    *loop for* task #16

--- loop_for.py
+++ loop_for.py
@@ -123,6 +123,22 @@ def Loops_Task9():
     
     print(total)
 
+def Loops_Task10():
+# На вход программе подаётся натуральное число n. 
+# Напишите программу, которая вычисляет значение выражения:
+# (1/1 + 1/2 + 1/3 + ... + 1/n) - ln(n)
+
+    from math import log
+
+    n = int(input())
+    result = 0
+
+    for i in range(1, n + 1):
+        result += 1 / i
+
+    print(result - log(n))
+
+
         
 
 

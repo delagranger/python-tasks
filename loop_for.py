28d49e277dc328531b49ace20dfa0937c4a48095
Auguste <longwonderstaff@gmail.com>
2026-04-15 13:13:41 +0300

    *loop for* task #22

--- loop_for.py
+++ loop_for.py
@@ -202,6 +202,21 @@ def Loops_Task15():
     result = "YES" if flag else "NO"
     print(result)
 
+def Loops_Task15():
+# На вход программе подаётся натуральное число n. 
+# Напишите программу вычисления знакочередующейся суммы
+
+    n = int(input())
+    total = 0
+
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            total -= i
+        else:
+            total += i
+    
+    print(total)
+
 
 
         

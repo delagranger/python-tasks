d4906fc04a862250e94f1cfda77e45320047e1a7
Auguste <longwonderstaff@gmail.com>
2026-04-19 23:53:08 +0300

    *loops* task #7

--- loops.py
+++ loops.py
@@ -1,13 +1,19 @@
-def Loops_Task28():
+def Loops_Task29():
 # Дано натуральное число. 
-# Напишите программу, которая выводит его цифры в столбик в обратном порядке.
+# Напишите программу, которая меняет порядок цифр числа на обратный.
 
     n = int(input())
+    n_new = ""
 
     while n != 0:
-        last_dig = n % 10
-        print(last_dig)
+        last_digit = str(n % 10)
+        n_new += last_digit
         n //= 10
+    
+    print(n_new)
+
+
+
 
 
 

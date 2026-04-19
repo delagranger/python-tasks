0a4bd56e9d2bd76a297a77d60a121d7e1e607896
Auguste <longwonderstaff@gmail.com>
2026-04-20 00:03:01 +0300

    *loops* task #8

--- loops.py
+++ loops.py
@@ -1,16 +1,27 @@
 def Loops_Task29():
-# Дано натуральное число. 
-# Напишите программу, которая меняет порядок цифр числа на обратный.
+# Дано натуральное число n
+# Напишите программу, которая определяет его максимальную и минимальную цифры
 
     n = int(input())
-    n_new = ""
+    max_dig = n % 10
+    min_dig = n % 10
 
     while n != 0:
-        last_digit = str(n % 10)
-        n_new += last_digit
+        last_dig = n % 10
+
+        if last_dig > max_dig:
+            max_dig = last_dig
+        elif last_dig < min_dig:
+            min_dig = last_dig
+        
         n //= 10
     
-    print(n_new)
+    print(f"Максимальная цифра равна {max_dig}")
+    print(f"Минимальная цифра равна {min_dig}")
+        
+        
+    
+
 
 
 

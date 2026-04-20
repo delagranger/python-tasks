1f0734b79bbb302773b7a7955249912d83e32a9c
Auguste <longwonderstaff@gmail.com>
2026-04-20 14:12:50 +0300

    *loops* task #11

--- loops.py
+++ loops.py
@@ -1,14 +1,25 @@
-def Loops_Task31():
-# Дано натуральное число n (n > 9). 
-# Напишите программу, которая определяет его вторую (с начала) цифру.
+def Loops_Task32():
+# Дано натуральное число. 
+# Напишите программу, которая определяет, состоит ли указанное число из одинаковых цифр.
 
     n = int(input())
+    flag = True
+    last_dig = n % 10
+    n //= 10
 
-    while len(str(n)) >= 2:
-        last_digit = n % 10
+    while n != 0:
+        curr_dig = n % 10
+
+        if curr_dig != last_dig:
+            flag = False
+            break
+
+        last_dig = curr_dig
         n //= 10
     
-    print(last_digit)
+    print("YES" if flag else "NO")
+
+
     
 
 
